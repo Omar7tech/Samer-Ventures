f788345ecb506e2c6f16ce755ea98f948f29d83a
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-25 15:40:30 +0300

    hero

--- resources/js/sections/HeroSection.tsx
+++ resources/js/sections/HeroSection.tsx
@@ -1,7 +1,14 @@
 const HeroSection = () => {
   return (
-    <div className="p-5">
-      <img className="w-full aspect-video rounded-lg object-cover" src="/hero-bg.jpg" alt="" />
+    <div className="p-5 relative">
+      <img className="w-full aspect-video min-h-[600px] h-[calc(100vh-4rem)] rounded-3xl object-cover" src="/hero-bg.jpg" alt="" />
+      <div className="absolute inset-0 flex items-center px-32">
+        <h1 className="text-white text-8xl font-extrabold leading-tight tracking-wider">
+          WE ARE YOUR<br />
+          SALES & BUSINESS<br />
+          VENTURES
+        </h1>
+      </div>
     </div>
   )
 }
