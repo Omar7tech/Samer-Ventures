5da3164d92d66b7797f49f046965615b30513b77
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-28 15:45:35 +0300

    make small sentence responsive

--- resources/js/sections/smallSentence.tsx
+++ resources/js/sections/smallSentence.tsx
@@ -1,6 +1,6 @@
 const smallSentence = () => {
   return (
-    <div className='text-center text-sm max-w-3xl mx-auto py-10'>
+    <div className='text-center text-xs sm:text-sm max-w-3xl mx-auto py-8 md:py-15 px-6 md:px-0 text-black leading-relaxed'>
       Samer Ventures combines years of experience in sales, customer relations, and business communication across
       insurance, automotive, enterprise services, and client-facing industries — helping businesses create stronger
       partnerships, better opportunities, and structured commercial growth.
