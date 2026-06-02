f762f3d174ca4f3aa326600c822151502df25712
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-02 09:59:41 +0300

    make the service intro better

--- resources/js/sections/ServicesIntro.tsx
+++ resources/js/sections/ServicesIntro.tsx
@@ -1,9 +1,9 @@
 const ServicesIntro = () => {
   return (
-    <section className="w-full py-12 md:py-16 px-6 md:px-32 lg:px-48 xl:px-64 font-inter">
+    <section className="w-full py-12 md:py-16 px-6 md:px-36  font-inter">
       <div className="max-w-[1700px] mx-auto">
-        <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-light text-gray-300 leading-relaxed text-center tracking-tight">
-          Whether You're Looking To Generate Opportunities, Build Partnerships, Strengthen Your Sales Efforts, Or Gain Market Insights, Samer Ventures Provides Flexible Growth Support Tailored To Your Business Goals.
+        <p className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-medium text-gray-300 tracking-wider leading-tight">
+          Whether You're <span className="text-gray-500">Looking</span> To Generate Opportunities, Build Partnerships, Strengthen Your Sales Efforts, <span className="text-gray-500">Or Gain Market Insights</span>, Samer Ventures Provides Flexible Growth Support Tailored To Your Business Goals.
         </p>
       </div>
     </section>
