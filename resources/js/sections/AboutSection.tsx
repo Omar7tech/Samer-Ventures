c5969ef8ccacd34486aeaf1ef9fb71499b8c54d0
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-05 18:49:44 +0300

    npm run lint

--- resources/js/sections/AboutSection.tsx
+++ resources/js/sections/AboutSection.tsx
@@ -1,6 +1,6 @@
+import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
 import { useRef } from 'react';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
